036ba1f4f24b7fca3d7c004e69adb1fc920148eb
souadhimel <souad.anam@gmail.com>
2022-06-09 17:44:45 +0600

    php basics

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo("My first php learning attempt.<br/>");
+echo("Hi there, I am Himel <br/> ");
+echo("I am very much excited to learn php. <br/>");
+echo("That is why I have admitted in a course<br/>");
+echo("I am not sure that can I complete the course in time or not!<br/>");
+echo("But I will try my level best to learn it. <br/>");
+echo("Actually nothing is impossible if you are honest in your approach. <br/>");
+echo("I will try my level best to learn it. <br/>");
+echo("I have decided to learn every day with small step up. <br/>");
+echo("That is how I am going to finish my homework. <br/>");
+print("Hey, I am using print !!!!!! <br/>");
+// var_dump();
+// print_r();
+
+$name='Himel';
+$country='Bangladesh';
+$city='Dhaka';
+$team='Portugal';
+$star='CR7';
+
+echo $name;
+echo $country;
+echo $city;
+echo $team;
+echo $star;
+echo $name. $country. $city. $team. $star;
+?>
